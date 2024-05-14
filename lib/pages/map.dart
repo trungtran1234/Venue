931d347e020d59d6df6df8b3820233379c8b6e48
Trung Tran <trungtran63470@gmail.com>
2024-05-13 22:34:49 -0700

    added info window and friend only events

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -9,6 +9,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:custom_info_window/custom_info_window.dart';
 import 'package:clippy_flutter/triangle.dart';
+enum EventVisibility { public, friendsOnly }
 
 
 class MapPage extends StatefulWidget {
@@ -26,6 +27,7 @@ class MapPageState extends State<MapPage>
   LatLng? currentPosition;
   Map<String, Map<String, dynamic>> userCache = {};
   CustomInfoWindowController _customInfoWindowController = CustomInfoWindowController();
+  
 
   @override
   bool get wantKeepAlive => true;
@@ -93,25 +95,54 @@ class MapPageState extends State<MapPage>
     });
   }
 
+  
+
 
   void _loadEvents() async {
-    FirebaseFirestore.instance
-      .collection('events')
-      .orderBy('createdAt', descending: true)
-      .snapshots()
-      .listen((snapshot) {
-        var newMarkers = <Marker>{};
-        for (var doc in snapshot.docs) {
-          double lat = doc.data()['latitude'];
-          double lng = doc.data()['longitude'];
-          var markerId = MarkerId(doc.id);
+  final user = FirebaseAuth.instance.currentUser;
+  List<String> friendsList = [];
 
+  if (user != null) {
+    var userDoc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+    if (userDoc.exists && userDoc.data()!.containsKey('friends')) {
+      friendsList = List<String>.from(userDoc.data()!['friends']);
+    }
+  }
+
+  FirebaseFirestore.instance
+    .collection('events')
+    .orderBy('createdAt', descending: true)
+    .snapshots()
+    .listen((snapshot) {
+      var newMarkers = <Marker>{};
+      for (var doc in snapshot.docs) {
+        double lat = doc.data()['latitude'];
+        double lng = doc.data()['longitude'];
+        String eventVisibility = doc.data()['visibility'];
+        String eventCreatorId = doc.data()['userId'];
+
+        bool shouldDisplay = false;
+
+        if (eventCreatorId == user?.uid) {
+          shouldDisplay = true;
+        } else {
+          switch (eventVisibility) {
+            case 'public':
+              shouldDisplay = true;
+              break;
+            case 'friendsOnly':
+              shouldDisplay = friendsList.contains(eventCreatorId);
+              break;
+          }
+        }
+
+        if (shouldDisplay) {
+          var markerId = MarkerId(doc.id);
           var marker = Marker(
             markerId: markerId,
             position: LatLng(lat, lng),
             onTap: () {
-              // Constructing detailed and styled custom info window
-              _customInfoWindowController.addInfoWindow!(
+               _customInfoWindowController.addInfoWindow!(
                 Column(
                 children: [
                   Expanded(
@@ -123,7 +154,7 @@ class MapPageState extends State<MapPage>
                         borderRadius: BorderRadius.circular(4),
                       ),
                       child: Padding(
-                        padding: const EdgeInsets.fromLTRB(10.0, 0, 4.0, 0), // Adjust the padding here
+                        padding: const EdgeInsets.fromLTRB(10.0, 0, 4.0, 0),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
@@ -141,7 +172,7 @@ class MapPageState extends State<MapPage>
                                       ),
                             ),
                               SizedBox(
-                              height: 8.0, // Adjust the height here
+                              height: 8.0, 
                             ),
                             Text(
                               doc.data()['title'],
@@ -169,7 +200,7 @@ class MapPageState extends State<MapPage>
                               style:
                                 Theme.of(context).textTheme.headline6?.copyWith(
                                     color: Colors.white,
-                                    fontSize: 14, // Adjust the font size here
+                                    fontSize: 14,
                                   ),
                               ),
                               Text(
@@ -177,9 +208,16 @@ class MapPageState extends State<MapPage>
                               style:
                                 Theme.of(context).textTheme.headline6?.copyWith(
                                     color: Colors.white,
-                                    fontSize: 14, // Adjust the font size here
+                                    fontSize: 14
                                   ),
                               ),
+                              Text(
+                                'Visibility: ${doc.data()['visibility'] == 'friendsOnly' ? 'Friends Only' : 'Public'}',
+                                style: Theme.of(context).textTheme.headline6?.copyWith(
+                                  color: Colors.white,
+                                  fontSize: 14, 
+                                ),
+                              )
                           
                           ],
                         ),
@@ -203,9 +241,12 @@ class MapPageState extends State<MapPage>
           );
           newMarkers.add(marker);
         }
-        setState(() => _markers = newMarkers);
-      });
-  }
+      }
+      setState(() => _markers = newMarkers);
+    });
+}
+
+
 
   void _addMarker(LatLng position, String address, DateTime? startDateTime,
       DateTime? endDateTime) {
@@ -223,145 +264,155 @@ class MapPageState extends State<MapPage>
     });
   }
 
-  void _onMapLongPress(LatLng position) async {
-    String address =
-        await getPlaceAddress(position.latitude, position.longitude);
-    DateTime? selectedStartDate;
-    DateTime? selectedEndDate;
+void _onMapLongPress(LatLng position) async {
+  String address = await getPlaceAddress(position.latitude, position.longitude);
+  DateTime? selectedStartDate;
+  DateTime? selectedEndDate;
+  EventVisibility visibility = EventVisibility.public;
 
-    final TextEditingController titleController = TextEditingController();
-    final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
 
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text('Create Event'),
-        content: StatefulBuilder(
-          builder: (BuildContext context, StateSetter setState) {
-            return SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextField(
-                      controller: titleController,
-                      decoration: InputDecoration(labelText: 'Title')),
-                  TextField(
-                      controller: descriptionController,
-                      decoration: InputDecoration(labelText: 'Description')),
-                  TextFormField(initialValue: address, readOnly: true),
-                  ListTile(
-                    title: Text('Select Start Date and Time'),
-                    subtitle: Text(selectedStartDate == null
-                        ? 'No date and time chosen'
-                        : DateFormat('yyyy-MM-dd – kk:mm')
-                                .format(selectedStartDate!) +
-                            ' hrs'),
-                    onTap: () async {
-                      final date = await showDatePicker(
+  final List<DropdownMenuItem<EventVisibility>> dropdownItems = [
+    DropdownMenuItem(value: EventVisibility.public, child: Text('Public')),
+    DropdownMenuItem(value: EventVisibility.friendsOnly, child: Text('Friends Only')),
+    ];
+
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text('Create Event'),
+      content: StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TextField(
+                    controller: titleController,
+                    decoration: InputDecoration(labelText: 'Title')),
+                TextField(
+                    controller: descriptionController,
+                    decoration: InputDecoration(labelText: 'Description')),
+                TextFormField(initialValue: address, readOnly: true),
+                ListTile(
+                  title: Text('Select Start Date and Time'),
+                  subtitle: Text(selectedStartDate == null
+                      ? 'No date and time chosen'
+                      : DateFormat('yyyy-MM-dd – kk:mm').format(selectedStartDate!) + ' hrs'),
+                  onTap: () async {
+                    final date = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime(2100),
+                    );
+                    if (date != null) {
+                      final time = await showTimePicker(
                         context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime.now(),
-                        lastDate: DateTime(2100),
+                        initialTime: TimeOfDay.now(),
                       );
-                      if (date != null) {
-                        final time = await showTimePicker(
-                          context: context,
-                          initialTime: TimeOfDay.now(),
-                        );
-                        if (time != null) {
-                          setState(() {
-                            selectedStartDate = DateTime(
-                              date.year,
-                              date.month,
-                              date.day,
-                              time.hour,
-                              time.minute,
-                            );
-                          });
-                        }
+                      if (time != null) {
+                        setState(() {
+                          selectedStartDate = DateTime(
+                            date.year,
+                            date.month,
+                            date.day,
+                            time.hour,
+                            time.minute,
+                          );
+                        });
                       }
-                    },
-                  ),
-                  ListTile(
-                    title: Text('Select End Date and Time'),
-                    subtitle: Text(selectedEndDate == null
-                        ? 'No date and time chosen'
-                        : DateFormat('yyyy-MM-dd – kk:mm')
-                                .format(selectedEndDate!) +
-                            ' hrs'),
-                    onTap: () async {
-                      final date = await showDatePicker(
+                    }
+                  },
+                ),
+                ListTile(
+                  title: Text('Select End Date and Time'),
+                  subtitle: Text(selectedEndDate == null
+                      ? 'No date and time chosen'
+                      : DateFormat('yyyy-MM-dd – kk:mm').format(selectedEndDate!) + ' hrs'),
+                  onTap: () async {
+                    final date = await showDatePicker(
+                      context: context,
+                      initialDate: selectedStartDate ?? DateTime.now(),
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime(2100),
+                    );
+                    if (date != null) {
+                      final time = await showTimePicker(
                         context: context,
-                        initialDate: selectedStartDate ?? DateTime.now(),
-                        firstDate: DateTime.now(),
-                        lastDate: DateTime(2100),
+                        initialTime: TimeOfDay.now(),
                       );
-                      if (date != null) {
-                        final time = await showTimePicker(
-                          context: context,
-                          initialTime: TimeOfDay.now(),
-                        );
-                        if (time != null) {
-                          setState(() {
-                            selectedEndDate = DateTime(
-                              date.year,
-                              date.month,
-                              date.day,
-                              time.hour,
-                              time.minute,
-                            );
-                          });
-                        }
+                      if (time != null) {
+                        setState(() {
+                          selectedEndDate = DateTime(
+                            date.year,
+                            date.month,
+                            date.day,
+                            time.hour,
+                            time.minute,
+                          );
+                        });
                       }
-                    },
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
-        actions: <Widget>[
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                _addMarker(
-                    position, address, selectedStartDate, selectedEndDate);
-                _saveEventToFirestore(
-                    position,
-                    titleController.text,
-                    descriptionController.text,
-                    address,
-                    selectedStartDate,
-                    selectedEndDate);
-              },
-              child: const Text('Submit')),
-        ],
+                    }
+                  },
+                ),
+                DropdownButton<EventVisibility>(
+                  value: visibility,
+                  onChanged: (EventVisibility? newValue) {
+                    setState(() {
+                      visibility = newValue!;
+                    });
+                  },
+                  items: dropdownItems,
+                ),
+              ],
+            ),
+          );
+        },
       ),
-    );
-  }
+      actions: <Widget>[
+        TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              _addMarker(
+                  position, address, selectedStartDate, selectedEndDate);
+              _saveEventToFirestore(
+                  position,
+                  titleController.text,
+                  descriptionController.text,
+                  address,
+                  selectedStartDate,
+                  selectedEndDate,
+                  visibility);
+            },
+            child: const Text('Submit')),
+      ],
+    ),
+  );
+}
+
+
 
 void _saveEventToFirestore(LatLng position, String title, String description,
-    String address, DateTime? startDateTime, DateTime? endDateTime) async {
+    String address, DateTime? startDateTime, DateTime? endDateTime, EventVisibility visibility) async {
   final user = FirebaseAuth.instance.currentUser;
   if (user != null) {
-    // Fetch user details from the cache or Firestore
+
     Map<String, dynamic> userDetails = userCache[user.uid] ?? {};
 
-    // Fetch from Firestore if not in cache
     if (userDetails.isEmpty) {
       var userDoc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
       if (userDoc.exists) {
         userDetails = userDoc.data()!;
-        userCache[user.uid] = userDetails; // Cache it for future use
+        userCache[user.uid] = userDetails;
       }
     }
 
-    // Extract the required user details
-    String username = userDetails['username'] ?? 'Unknown'; // Make sure the key matches your database
+    String username = userDetails['username'] ?? 'Unknown'; 
     String firstName = userDetails['firstName'] ?? 'Unknown';
     String lastName = userDetails['lastName'] ?? 'Unknown';
 
-    // Now add these details along with the event details to Firestore
     FirebaseFirestore.instance.collection('events').add({
       'userId': user.uid,
       'userEmail': user.email,
@@ -376,6 +427,7 @@ void _saveEventToFirestore(LatLng position, String title, String description,
       'startDateTime': startDateTime?.toIso8601String(),
       'endDateTime': endDateTime?.toIso8601String(),
       'createdAt': FieldValue.serverTimestamp(),
+      'visibility': visibility.toString().split('.').last,
     });
   }
 }
@@ -418,7 +470,7 @@ void _saveEventToFirestore(LatLng position, String title, String description,
           ),
         ],
       ),
-      bottomNavigationBar: buildBottomNavigationBar(context, 1), // Assuming you have this function
+      bottomNavigationBar: buildBottomNavigationBar(context, 1),
     );
   }
 }
